b8ca432518108add45402aa4bec69f6eff5b8fa3
MickeyWang <readmeok@126.com>
2017-07-21 20:15:05 +0800

    session+register

--- app.js
+++ app.js
@@ -9,12 +9,34 @@ var bodyParser = require('body-parser');
 // var users = require('./routes/users');
 var routes = require('./routes/index');
 var settings = require('./settings');
+var flash = require('connect-flash');
+
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
 var app = express();
+
+app.use(session({
+    secret: settings.cookieSecret,
+    key: settings.db, //cookie name
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 30
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new MongoStore({
+        // db: settings.db,
+        // host: settings.host,
+        // port: settings.port
+        url: settings.url
+    })
+
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(flash());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/index.js
+++ routes/index.js
@@ -8,14 +8,64 @@
 //
 // module.exports = router;
 
+var crypto = require('crypto'),
+    User = require('../models/user.js');
+
 module.exports = function (app) {
     app.get('/', function (req, res) {
-        res.render('index', {title: '主页'});
+        res.render('index', {
+            title: '主页',
+            user: req.session.user,
+            success: req.flash('success').toString(),
+            error: req.flash('error').toString()
+        });
     });
     app.get('/reg', function (req, res) {
-        res.render('reg', {title: '注册'});
+        res.render('reg', {
+            title: '注册',
+            user: req.session.user,
+            success: req.flash('sucess').toString(),
+            error: req.flash('error').toString()
+        });
     });
     app.post('/reg', function (req, res) {
+        var name = req.body.name,
+            password = req.body.password,
+            password_re = req.body['password-repeat'];
+        // 检验用户两次输入的密码是否一致
+        if(password_re != password){
+            req.flash('error', '两次输入的密码不一致！');
+            return res.redirect('/reg');//返回注册页
+        }
+        // 生成密码的md5值
+        var md5 = crypto.createHash('md5'),
+            password = md5.update(req.body.password).digest('hex');
+        var newUser = new User({
+            name: req.body.name,
+            password: password,
+            email: req.body.email
+        });
+        // 检查用户名是否已经存在
+        User.get(newUser.name, function (err, user) {
+            if(err){
+                req.flash('error', err);
+                return res.redirect('/');
+            }
+            if(user) {
+                req.flash('error', '用户已存在！');
+                return res.redirect('/reg');
+            }
+            // 如果不存在则新增用户
+            newUser.save(function (err, user) {
+                if(err) {
+                    req.flash('error', err);
+                    return res.redirect('/reg');
+                }
+                req.session.user = user;
+                req.flash('success', '注册成功！');
+                res.redirect('/');
+            })
+        })
     });
     app.get('/login', function (req, res) {
         res.render('login', {title: '登录'});
