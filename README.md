7457f291558638aa81a73563bdba160677e1e338
MickeyWang <readmeok@126.com>
2017-08-08 15:32:52 +0800

    添加转载功能及转载统计功能

--- README.md
+++ README.md
@@ -10,3 +10,4 @@
  - ejs遇到esc报错信息
  - package.json中的版本问题
  - 注：$符号开头的均为mongoDB操作符，应去mongoDB文档查询
+ - chrome中收藏标签
--- models/post.js
+++ models/post.js
@@ -24,7 +24,7 @@ Post.prototype.save = function (callback) {
         month: date.getFullYear() + '-' + (date.getMonth() + 1),
         day: date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate(),
         minute: date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ':' + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes())
-    }
+    };
     // 要存入数据库的文档
     var post = {
         name: this.name,
@@ -34,6 +34,7 @@ Post.prototype.save = function (callback) {
         tags: this.tags,
         post: this.post,
         comments: [],
+        reprint_info: {}, // 转载记录
         pv: 0
     }
 
@@ -105,18 +106,18 @@ Post.getTen = function (name, pageNo, callback) {
 // 获取所有标签
 Post.getTags = function (callback) {
     mongodb.open(function (err, db) {
-        if(err){
+        if (err) {
             return callback(err);
         }
         db.collection('posts', function (err, collection) {
-            if(err){
+            if (err) {
                 mongodb.close();
                 return callback(err);
             }
             //The distinct command returns a list of distinct values for the given key across a collection.
-            collection.distinct('tags',function (err, docs) {
+            collection.distinct('tags', function (err, docs) {
                 mongodb.close();
-                if(err){
+                if (err) {
                     return callback(err);
                 }
                 callback(null, docs);// 返回的docs就是只包含不重复tags值的一个数组
@@ -128,11 +129,11 @@ Post.getTags = function (callback) {
 // 获取特定标签的所有文章
 Post.getTag = function (tag, callback) {
     mongodb.open(function (err, db) {
-        if(err){
+        if (err) {
             return callback(err);
         }
         db.collection('posts', function (err, collection) {
-            if(err){
+            if (err) {
                 mogodb.close();
                 return callback(err);
             }
@@ -148,7 +149,7 @@ Post.getTag = function (tag, callback) {
                 time: -1
             }).toArray(function (err, docs) {
                 mongodb.close();
-                if(err){
+                if (err) {
                     return callback(err);
                 }
                 callback(null, docs);
@@ -157,7 +158,7 @@ Post.getTag = function (tag, callback) {
     })
 }
 
-// 获取一篇文章
+// 获取一篇文章（获取到解析过后的html格式）
 Post.getOne = function (name, day, title, callback) {
     mongodb.open(function (err, db) {
         if (err) {
@@ -185,9 +186,9 @@ Post.getOne = function (name, day, title, callback) {
                         "title": title
                     }, {
                         $inc: {'pv': 1} // Increments the value of the field by the specified amount.
-                    },function (err) {
+                    }, function (err) {
                         mongodb.close();
-                        if(err){
+                        if (err) {
                             return callback(err);
                         }
                     });
@@ -204,7 +205,7 @@ Post.getOne = function (name, day, title, callback) {
 
         })
     })
-}
+};
 
 // 返回原始发表的内容（markdown源格式）
 Post.edit = function (name, day, title, callback) {
@@ -232,7 +233,7 @@ Post.edit = function (name, day, title, callback) {
             })
         })
     })
-}
+};
 
 // 更新一篇文章及相关信息
 Post.update = function (name, day, title, post, callback) {
@@ -260,7 +261,7 @@ Post.update = function (name, day, title, post, callback) {
             })
         })
     })
-}
+};
 
 // 删除一篇文章
 Post.remove = function (name, day, title, callback) {
@@ -273,31 +274,69 @@ Post.remove = function (name, day, title, callback) {
                 mongodb.close();
                 return callback(err);
             }
-            collection.deleteOne({
+
+            // 查询要删除的文档
+            collection.findOne({
                 'name': name,
                 'time.day': day,
                 'title': title
-            }, {w: 1}, function (err, result) {
-                mongodb.close();
-                if (err) {
+            }, function (err, doc) {
+                if(err){
+                    mongodb.close();
                     return callback(err);
-                } else if (result.result.n == 0) {
-                    return callback(new Error('删除失败！'))
                 }
-                callback(null);
-            })
-        })
-    })
-}
+                // 如果有reprint_from,即该文章是转载来的，先保存下来reprint_from，以便后续更新原文中reprint_to的信息
+                var reprint_from = '';
+                if(doc.reprint_info.reprint_from){
+                    reprint_from = doc.reprint_info.reprint_from;
+                    // 更新原文章所在文档的reprint_to
+                    collection.updateOne({
+                        'name': reprint_from.name,
+                        'time.day': reprint_from.day,
+                        'title': reprint_from.title
+                    }, {
+                        $pull: {
+                            'reprint_info.reprint_to': {
+                                'name': name,
+                                'day': day,
+                                'title': title
+                            }
+                        }
+                    }, function (err) {
+                        if(err){
+                            mongodb.close();
+                            return callback(err);
+                        }
+                    });
+                }
+
+                // 删除转载来的文章所在的文档
+                collection.deleteOne({
+                    'name': name,
+                    'time.day': day,
+                    'title': title
+                }, {w: 1}, function (err, result) {
+                    mongodb.close();
+                    if (err) {
+                        return callback(err);
+                    } else if (result.result.n == 0) {
+                        return callback(new Error('删除失败！'))
+                    }
+                    callback(null);
+                });
+            });
+        });
+    });
+};
 
 // 返回所有文章的存档信息
 Post.getArchive = function (callback) {
     mongodb.open(function (err, db) {
-        if(err){
+        if (err) {
             return callback(err);
         }
         db.collection('posts', function (err, collection) {
-            if(err){
+            if (err) {
                 mongodb.close();
                 return callback(err);
             }
@@ -310,7 +349,7 @@ Post.getArchive = function (callback) {
                 time: -1
             }).toArray(function (err, docs) {
                 mongodb.close();
-                if(err){
+                if (err) {
                     return callback(err);
                 }
                 callback(null, docs);
@@ -322,11 +361,11 @@ Post.getArchive = function (callback) {
 // 通过标题关键字模糊查询文章信息（即支持正则）
 Post.search = function (keyword, callback) {
     mongodb.open(function (err, db) {
-        if(err){
+        if (err) {
             return callback(err);
         }
-        db.collection('posts',function (err, collection) {
-            if(err){
+        db.collection('posts', function (err, collection) {
+            if (err) {
                 mongodb.close();
                 return callback(err);
             }
@@ -341,12 +380,100 @@ Post.search = function (keyword, callback) {
                 time: -1
             }).toArray(function (err, docs) {
                 mongodb.close();
-                if(err){
+                if (err) {
                     return callback(err);
                 }
                 callback(null, docs);
             });
         })
     })
-}
+};
+
+/* 转载一篇文章
+ reprint_info的格式类似如下：
+ {
+ reprint_from: {name: xxx, day: xxx, title: xxx}, // 代表此文章是转载来自于···
+ reprint_to: [ // 代表此文章是被转载到···
+ {name: xxx, day: xxx, title: xxx},
+ {name: xxx, day: xxx, title: xxx},
+ ···
+ ]
+ }
+
+ 注意：需要改造Post.remove
+ */
+Post.reprint = function (reprint_from, reprint_to, callback) {
+    mongodb.open(function (err, db) {
+        if (err) {
+            return callback(err);
+        }
+        db.collection('posts', function (err, collection) {
+            if (err) {
+                mongodb.close();
+                return callback(err);
+            }
+            // 1、找到被转载的文章的原文档，并及修改部分要素生成新的副本
+            collection.findOne({
+                'name': reprint_from.name,
+                'title': reprint_from.title,
+                'time.day': reprint_from.day
+            }, function (err, doc) {
+                if (err) {
+                    mongodb.close();
+                    return callback(err);
+                }
+
+                var date = new Date();
+                var time = {
+                    date: date,
+                    year: date.getFullYear(),
+                    month: date.getFullYear() + '-' + (date.getMonth() + 1),
+                    day: date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate(),
+                    minute: date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ':' + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes())
+                };
+
+                delete doc._id;// 注意要删掉原来的_id
+
+                doc.name = reprint_to.name;
+                doc.head = reprint_to.head;
+                doc.time = time;
+                doc.title = (doc.title.search(/[转载]/) > -1) ? doc.title : "[转载]" + doc.title;
+                doc.comments = [];
+                doc.reprint_info = {'reprint_from': reprint_from};
+                doc.pv = 0;
+
+                // 2、更新被转载文章的原文档，将reprint_info中的reprint_to更新
+                collection.updateOne({
+                    'name': reprint_from.name,
+                    'title': reprint_from.title,
+                    'time.day': reprint_from.day
+                },{
+                    $push: {
+                        'reprint_info.reprint_to': {
+                            'name': doc.name,
+                            'day': time.day,
+                            'title': doc.title
+                        }
+                    }
+                }, function (err) {
+                    if(err){
+                        mongodb.close();
+                        return callback(err);
+                    }
+                });
+
+                // 3、将转载修改后生成的副本存入数据库，并返回存储后的文档
+                collection.insertOne(doc, {
+                    w: 1
+                }, function (err, post) {
+                    mongodb.close();
+                    if(err){
+                        return callback(err);
+                    }
+                    callback(err, post.ops[0]);
+                });
+            })
+        })
+    })
+};
 
